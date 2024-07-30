fcf5f9818e4104fee2b70d07b75be993fde73d4b
cuongh1799 <cuongghuynhh1009@gmail.com>
2024-07-30 21:08:07 +0700

    improve UI menu appearance

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -33,7 +33,6 @@
             flowLayoutPanel1 = new FlowLayoutPanel();
             MaterialButton = new Button();
             BuildButton = new Button();
-            MaterialPanel = new RichTextBox();
             addCharacterButton = new Button();
             sidebar = new FlowLayoutPanel();
             RefreshButton = new Button();
@@ -46,100 +45,118 @@
             // 
             pictureBox1.BackColor = Color.Transparent;
             pictureBox1.BackgroundImageLayout = ImageLayout.None;
-            pictureBox1.Location = new Point(404, 12);
+            pictureBox1.Location = new Point(374, 16);
+            pictureBox1.Margin = new Padding(3, 4, 3, 4);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(732, 533);
+            pictureBox1.Size = new Size(925, 801);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 1;
             pictureBox1.TabStop = false;
             // 
             // flowLayoutPanel1
             // 
+            flowLayoutPanel1.BackColor = Color.FromArgb(46, 43, 39);
             flowLayoutPanel1.Controls.Add(MaterialButton);
             flowLayoutPanel1.Controls.Add(BuildButton);
-            flowLayoutPanel1.Location = new Point(337, 619);
+            flowLayoutPanel1.Location = new Point(374, 825);
+            flowLayoutPanel1.Margin = new Padding(3, 4, 3, 4);
             flowLayoutPanel1.Name = "flowLayoutPanel1";
-            flowLayoutPanel1.Size = new Size(803, 100);
+            flowLayoutPanel1.Size = new Size(929, 133);
             flowLayoutPanel1.TabIndex = 2;
             // 
             // MaterialButton
             // 
-            MaterialButton.Location = new Point(3, 3);
+            MaterialButton.BackColor = Color.FromArgb(97, 86, 67);
+            MaterialButton.FlatAppearance.BorderSize = 0;
+            MaterialButton.FlatStyle = FlatStyle.Flat;
+            MaterialButton.Font = new Font("Segoe UI", 20F);
+            MaterialButton.ForeColor = SystemColors.ButtonFace;
+            MaterialButton.Location = new Point(3, 4);
+            MaterialButton.Margin = new Padding(3, 4, 3, 4);
             MaterialButton.Name = "MaterialButton";
-            MaterialButton.Size = new Size(392, 97);
+            MaterialButton.Size = new Size(448, 129);
             MaterialButton.TabIndex = 0;
             MaterialButton.Text = "Material";
-            MaterialButton.UseVisualStyleBackColor = true;
+            MaterialButton.UseVisualStyleBackColor = false;
             MaterialButton.Click += BuildButton_Click;
             // 
             // BuildButton
             // 
-            BuildButton.Location = new Point(401, 3);
+            BuildButton.BackColor = Color.FromArgb(97, 86, 67);
+            BuildButton.FlatAppearance.BorderSize = 0;
+            BuildButton.FlatStyle = FlatStyle.Flat;
+            BuildButton.Font = new Font("Segoe UI", 20F);
+            BuildButton.ForeColor = SystemColors.ButtonFace;
+            BuildButton.Location = new Point(457, 4);
+            BuildButton.Margin = new Padding(3, 4, 3, 4);
             BuildButton.Name = "BuildButton";
-            BuildButton.Size = new Size(398, 97);
+            BuildButton.Size = new Size(455, 129);
             BuildButton.TabIndex = 1;
             BuildButton.Text = "Build";
-            BuildButton.UseVisualStyleBackColor = true;
-            // 
-            // MaterialPanel
-            // 
-            MaterialPanel.Font = new Font("Segoe UI", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            MaterialPanel.Location = new Point(641, 22);
-            MaterialPanel.Name = "MaterialPanel";
-            MaterialPanel.ReadOnly = true;
-            MaterialPanel.Size = new Size(487, 355);
-            MaterialPanel.TabIndex = 3;
-            MaterialPanel.Text = "";
-            MaterialPanel.Visible = false;
+            BuildButton.UseVisualStyleBackColor = false;
             // 
             // addCharacterButton
             // 
+            addCharacterButton.BackColor = Color.FromArgb(97, 86, 67);
             addCharacterButton.BackgroundImageLayout = ImageLayout.Center;
-            addCharacterButton.Location = new Point(3, 3);
+            addCharacterButton.FlatAppearance.BorderSize = 0;
+            addCharacterButton.FlatStyle = FlatStyle.Flat;
+            addCharacterButton.Font = new Font("Sitka Display", 20F);
+            addCharacterButton.ForeColor = SystemColors.ButtonFace;
+            addCharacterButton.Location = new Point(3, 4);
+            addCharacterButton.Margin = new Padding(3, 4, 3, 4);
             addCharacterButton.Name = "addCharacterButton";
-            addCharacterButton.Size = new Size(309, 66);
+            addCharacterButton.Size = new Size(362, 88);
             addCharacterButton.TabIndex = 7;
             addCharacterButton.Text = "Add Character";
-            addCharacterButton.UseVisualStyleBackColor = true;
+            addCharacterButton.UseVisualStyleBackColor = false;
             addCharacterButton.Click += addCharacterButton_Click;
             // 
             // sidebar
             // 
             sidebar.AutoScroll = true;
-            sidebar.BorderStyle = BorderStyle.Fixed3D;
+            sidebar.BackColor = Color.FromArgb(46, 43, 39);
             sidebar.Controls.Add(addCharacterButton);
             sidebar.FlowDirection = FlowDirection.TopDown;
-            sidebar.Location = new Point(12, 12);
+            sidebar.Location = new Point(3, 91);
+            sidebar.Margin = new Padding(3, 4, 3, 4);
             sidebar.Name = "sidebar";
-            sidebar.Size = new Size(319, 695);
+            sidebar.Size = new Size(368, 867);
             sidebar.TabIndex = 8;
             sidebar.WrapContents = false;
             // 
             // RefreshButton
             // 
-            RefreshButton.Location = new Point(332, 12);
+            RefreshButton.BackColor = Color.FromArgb(97, 86, 67);
+            RefreshButton.FlatAppearance.BorderSize = 0;
+            RefreshButton.FlatStyle = FlatStyle.Flat;
+            RefreshButton.Font = new Font("Sitka Display", 20F);
+            RefreshButton.ForeColor = SystemColors.ButtonFace;
+            RefreshButton.Location = new Point(3, 13);
+            RefreshButton.Margin = new Padding(3, 4, 3, 4);
             RefreshButton.Name = "RefreshButton";
-            RefreshButton.Size = new Size(66, 71);
+            RefreshButton.Size = new Size(368, 76);
             RefreshButton.TabIndex = 9;
             RefreshButton.Text = "Refresh";
-            RefreshButton.UseVisualStyleBackColor = true;
+            RefreshButton.UseVisualStyleBackColor = false;
             RefreshButton.Click += RefreshButton_Click;
             // 
             // MainForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(1140, 719);
-            Controls.Add(RefreshButton);
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Center;
+            ClientSize = new Size(1303, 959);
             Controls.Add(sidebar);
-            Controls.Add(MaterialPanel);
+            Controls.Add(RefreshButton);
             Controls.Add(flowLayoutPanel1);
             Controls.Add(pictureBox1);
+            DoubleBuffered = true;
             Icon = (Icon)resources.GetObject("$this.Icon");
-            Margin = new Padding(3, 2, 3, 2);
             MaximizeBox = false;
-            MinimumSize = new Size(1156, 756);
+            MinimumSize = new Size(1319, 992);
             Name = "MainForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Changli/Reborn";
@@ -153,7 +170,6 @@
         private FlowLayoutPanel flowLayoutPanel1;
         private Button MaterialButton;
         private Button BuildButton;
-        private RichTextBox MaterialPanel;
         private Button addCharacterButton;
         private FlowLayoutPanel sidebar;
         public PictureBox pictureBox1;
